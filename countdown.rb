5a518239de8737ea969504f3b0e62e893ac2037d
Russell Doane <russdoane@yahoo.com>
2020-06-23 08:12:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,13 @@ end
   
     return "HAPPY NEW YEAR!"
 end 
+
+def countdown_with_sleep(number)
+  number = 10
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
